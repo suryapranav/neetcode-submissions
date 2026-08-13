eee74a82761b030f4222e6211d818e10521336df
Surya Pranav <thesuryapranav@gmail.com>
2026-08-14 01:50:07 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,27 @@
+class Solution:
+    # def cleaner(self, s:str) -> str:
+    #     out = []
+    #     for letter in s.lower():
+    #         if ((letter >= 'a' and letter <= 'z') or (letter >= 'A' and letter <= 'Z') or (letter >= '0' and letter <= '9')):
+    #             out.append(letter)
+    #     return "".join(out)
+    # def isPalindrome(self, s: str) -> bool:
+    #     comparer = self.cleaner(s)
+    #     print(comparer)
+    #     return (comparer[::-1] == comparer)
+
+    def isPalindrome(self, s:str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if not s[right].isalnum():
+                right -= 1
+                continue
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
